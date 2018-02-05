3efc78ef80a2eea8c4e7434bb8645341cc1ec351
zhang <hangisoft@163.com>
2018-02-05 14:53:30 +0800

    添加图片

---
+++ js/index.js
@@ -0,0 +1,30 @@
+$(function () {
+    var sec = 0;
+    var min = 0;
+    var timer = null;
+    $('.bottom button').on('click',function () {
+        $(this).text('游戏中');
+        console.log('点击');
+
+        timer = setInterval(function () {
+            sec++;
+            console.log('sec===='+sec);
+            if(sec < 10){
+                $('.second').text('0'+sec);
+            }else if(sec == 60){
+                min++;
+                if(min < 10 ){
+                    $('.minute').text('0'+min);
+                }else{
+                    $('.minute').text(min);
+                }
+                $('.second').text('00');
+                sec = 0;
+            }else{
+                $('.second').text(sec);
+            }
+        },1000);
+
+    })
+
+})
