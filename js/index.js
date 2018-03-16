cea886f58aed4dbf1c5ffca5c82e14d70dff726e
zhang <hangisoft@163.com>
2018-03-16 20:20:40 +0800

    判断将要移动的位置是否有数字

--- js/index.js
+++ js/index.js
@@ -50,21 +50,26 @@ $(function () {
     })
 
     //li的宽度
-    var liWidth = $('#main li').width();
-    var liHeight = $('#main li').height();
-    var left1 = $('#main').find('li').eq(0).offset().left;
-    var top1 = $('#main').find('li').eq(0).offset().top;
-    var left2 = $('#main').find('li').eq(1).offset().left;
-    var top2 = $('#main').find('li').eq(3).offset().top;
+    var left1 = parseInt($('#main').find('li').eq(0).offset().left);
+    var top1 = parseInt($('#main').find('li').eq(0).offset().top);
+    var left2 = parseInt($('#main').find('li').eq(1).offset().left);
+    var top2 = parseInt($('#main').find('li').eq(3).offset().top);
+    var left3 = parseInt($('#main').find('li').eq(2).offset().left);
+    var top3 = parseInt($('#main').find('li').eq(6).offset().top);
 
     console.log('left1====='+left1);
     console.log('left2====='+left2);
+    console.log('left3====='+left3);
     var distanceX = parseInt(left2 - left1);
     var distanceY = parseInt(top2 - top1);
     console.log('distanceY====='+distanceY);
-    //console.log('liWidth====='+liWidth);
-    //console.log('liHeight====='+liHeight);
 
+    //方向移动false  true水平方向移动  false竖直方向移动
+    var flag = false;
+    //水平方向移动flag  true向右移  false向左移
+    var hFlag = false;
+    //垂直方向移动flag true向上移  false向下移
+    var vFlag = false;
 
     var main = document.getElementById('main');
     var li = document.getElementsByTagName('li');
@@ -74,9 +79,9 @@ $(function () {
         event.preventDefault();
         startX = e.targetTouches[0].clientX;
         startY = e.targetTouches[0].clientY;
-        console.log(parseInt(startX));
-        console.log(parseInt(startY));
-        console.log(event.targetTouches[0].target.offsetLeft);
+        //console.log(parseInt(startX));
+        //console.log(parseInt(startY));
+        //console.log(event.targetTouches[0].target.offsetLeft);
     },false)
 
     main.addEventListener('touchmove',function (event) {
@@ -84,63 +89,76 @@ $(function () {
         moveX = parseInt(event.targetTouches[0].clientX);
         moveY = parseInt(event.targetTouches[0].clientY);
 
-        if(moveX - startX >= 30){
-            console.log(moveX - startX);
-            //event.targetTouches[0].target.offsetLeft += liWidth;
+        //水平方向拖动
+        if(moveX - startX >= 30 && event.targetTouches[0].target.offsetLeft < left3
+        && event.targetTouches[0].target.nextElementSibling.innerHTML == ''){
+            console.log(event.targetTouches[0].target.innerHTML);
+            console.log(event.targetTouches[0].target.nextElementSibling.innerHTML);
             event.targetTouches[0].target.style.transform = 'translateX('+distanceX+'px)';
-        }else if(moveX - startX < 0 && Math.abs(moveX - startX) >= 30){
+            hFlag = true;
+            flag = true;
+        }else if(moveX - startX < 0 && Math.abs(moveX - startX) >= 30
+        && event.targetTouches[0].target.offsetLeft >left1
+        && event.targetTouches[0].target.previousElementSibling.innerHTML == ''){
             event.targetTouches[0].target.style.transform = 'translateX('+-distanceX+'px)';
+            hFlag = false;
+            flag = true;
         }
 
-        if(moveY - startY >= 30){
+        //垂直方向拖动  拖动条件 1、移动距离大于30px  2、不能向边缘移动  3、将要移动到的位置没有数字
+        if(moveY - startY >= 30 && event.targetTouches[0].target.offsetTop < top3
+        && event.targetTouches[0].target.nextElementSibling.nextElementSibling.nextElementSibling.innerHTML == ''){
+            console.log(event.targetTouches[0].target.nextElementSibling.nextElementSibling.nextElementSibling.innerHTML);
+            console.log(event.targetTouches[0].target.nextElementSibling.nextElementSibling.nextElementSibling.innerHTML == null);
+            console.log($(event.targetTouches[0].target).text());
             event.targetTouches[0].target.style.transform = 'translateY('+distanceY+'px)';
-        }else if(moveY - startY < 0 && Math.abs(moveY - startY) >= 30){
+            vFlag = true;
+            flag = false;
+        }else if(moveY - startY < 0 && Math.abs(moveY - startY) >= 30
+        && event.targetTouches[0].target.offsetTop > top1 &&
+            event.targetTouches[0].target.previousElementSibling.previousElementSibling.previousElementSibling.innerHTML
+        == ''){
             event.targetTouches[0].target.style.transform = 'translateY('+-distanceY+'px)';
+            vFlag = false;
+            flag = false;
         }
-        //console.log(event.targetTouches[0].target.innerHTML);
-        //console.log(event.targetTouches[0].target.offsetLeft);
     },false);
 
+    main.addEventListener('touchend',function (event) {
+        event.preventDefault();
+        //console.log(event.target.innerHTML);
+        //console.log(event.target.nextElementSibling);
+        if(flag){
+            console.log(1);
+            if(hFlag){
+                event.target.nextElementSibling.innerHTML = event.target.innerHTML;
+                event.target.nextElementSibling.style.borderWidth = 1+'px';
+                event.target.nextElementSibling.style.borderColor = 'blue';
+            }else{
+                event.target.previousElementSibling.innerHTML = event.target.innerHTML;
+                event.target.previousElementSibling.style.borderWidth = 1+'px';
+                event.target.previousElementSibling.style.borderColor = 'blue';
+            }
+        }else{
+            console.log(2);
+            if(vFlag){
+                event.target.nextElementSibling.nextElementSibling.nextElementSibling.innerHTML = event.target.innerHTML;
+                event.target.nextElementSibling.nextElementSibling.nextElementSibling.style.borderWidth = 1+'px';
+                event.target.nextElementSibling.nextElementSibling.nextElementSibling.style.borderColor = 'blue';
+            }else{
+                event.target.previousElementSibling.previousElementSibling.previousElementSibling.innerHTML = event.target.innerHTML;
+                event.target.previousElementSibling.previousElementSibling.previousElementSibling.style.borderWidth = 1+'px';
+                event.target.previousElementSibling.previousElementSibling.previousElementSibling.style.borderColor = 'blue';
+            }
+        }
+
+        event.target.innerHTML = '';
+        event.target.removeAttribute("style");
+        event.target.style.borderWidth = 0+'px';
+
+    })
     var startX,startY,moveX,moveY;
 
- /*   $('#main ul li').on('touchstart',function(e){
-        e.preventDefault();
-        startX = e.targetTouches[0].clientX;
-        startY = e.targetTouches[0].clientY;
-        console.log(parseInt(startX));
-        console.log(parseInt(startY));
-        console.log($(this).text());
-    })*/
- /*   for(var i = 0; i < li.length; i++){
-        //var ind = this.i;
-        //console.log(i);
-        li[i].addEventListener('touchstart',function (event) {
-            //console.log(this.getElementsByTagName('img')[0].getAttribute('src'));
-           /!* console.log(this.getElementsByTagName('img')[0].offsetWidth);
-            console.log(this.offsetWidth);*!/
-            console.log(this.getElementsByTagName('img')[0].offsetLeft);
-            console.log(this.offsetLeft);
-           // console.log(this.getElementsByTagName('img')[0].getAttribute('src'));
-            console.log(parseInt(event.touches[0].clientX));
-            console.log(parseInt(event.touches[0].clientY));
-
-            console.log(event.touches[0].target);
-        },false);
-
-        li[i].addEventListener('touchmove',function (event) {
-            console.log(this.getElementsByTagName('img')[0].getAttribute('src'));
-            console.log('moveX====='+parseInt(event.targetTouches[0].clientX));
-            console.log('moveY====='+parseInt(event.targetTouches[0].clientY));
-            console.log(event.targetTouches[0].target);
-        },false);
-
-        li[i].addEventListener('touchend',function (event) {
-            console.log(this.getElementsByTagName('img')[0].getAttribute('src'));
-            //console.log(parseInt(event.changedTouches[0].clientX));
-            //console.log(parseInt(event.changedTouches[0].clientY));
-            //console.log(event.touches[0].target);
-        },false);
-    }*/
 
 
 })
