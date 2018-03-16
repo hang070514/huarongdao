e261b48add1b47df8e3eea58704a869e08567427
zhang <hangisoft@163.com>
2018-03-16 13:56:38 +0800

    数字移动

--- js/index.js
+++ js/index.js
@@ -45,16 +45,72 @@ $(function () {
     disOrder(numArr);
 
     numArr.forEach(function (value,index) {
-        console.log(value);
+        //console.log(value);
         $('#main li').eq(index).html(value);
     })
+
+    //li的宽度
+    var liWidth = $('#main li').width();
+    var liHeight = $('#main li').height();
+    var left1 = $('#main').find('li').eq(0).offset().left;
+    var top1 = $('#main').find('li').eq(0).offset().top;
+    var left2 = $('#main').find('li').eq(1).offset().left;
+    var top2 = $('#main').find('li').eq(3).offset().top;
+
+    console.log('left1====='+left1);
+    console.log('left2====='+left2);
+    var distanceX = parseInt(left2 - left1);
+    var distanceY = parseInt(top2 - top1);
+    console.log('distanceY====='+distanceY);
+    //console.log('liWidth====='+liWidth);
+    //console.log('liHeight====='+liHeight);
+
 
     var main = document.getElementById('main');
     var li = document.getElementsByTagName('li');
 
-    main.addEventListener('touchstart',function (event) {
-        console.log(this.getElementsByTagName('img')[0].getAttribute('src'));
+    main.addEventListener('touchstart',function (e) {
+        //console.log(this.getElementsByTagName('img')[0].getAttribute('src'));
+        event.preventDefault();
+        startX = e.targetTouches[0].clientX;
+        startY = e.targetTouches[0].clientY;
+        console.log(parseInt(startX));
+        console.log(parseInt(startY));
+        console.log(event.targetTouches[0].target.offsetLeft);
     },false)
+
+    main.addEventListener('touchmove',function (event) {
+        event.preventDefault();
+        moveX = parseInt(event.targetTouches[0].clientX);
+        moveY = parseInt(event.targetTouches[0].clientY);
+
+        if(moveX - startX >= 30){
+            console.log(moveX - startX);
+            //event.targetTouches[0].target.offsetLeft += liWidth;
+            event.targetTouches[0].target.style.transform = 'translateX('+distanceX+'px)';
+        }else if(moveX - startX < 0 && Math.abs(moveX - startX) >= 30){
+            event.targetTouches[0].target.style.transform = 'translateX('+-distanceX+'px)';
+        }
+
+        if(moveY - startY >= 30){
+            event.targetTouches[0].target.style.transform = 'translateY('+distanceY+'px)';
+        }else if(moveY - startY < 0 && Math.abs(moveY - startY) >= 30){
+            event.targetTouches[0].target.style.transform = 'translateY('+-distanceY+'px)';
+        }
+        //console.log(event.targetTouches[0].target.innerHTML);
+        //console.log(event.targetTouches[0].target.offsetLeft);
+    },false);
+
+    var startX,startY,moveX,moveY;
+
+ /*   $('#main ul li').on('touchstart',function(e){
+        e.preventDefault();
+        startX = e.targetTouches[0].clientX;
+        startY = e.targetTouches[0].clientY;
+        console.log(parseInt(startX));
+        console.log(parseInt(startY));
+        console.log($(this).text());
+    })*/
  /*   for(var i = 0; i < li.length; i++){
         //var ind = this.i;
         //console.log(i);
